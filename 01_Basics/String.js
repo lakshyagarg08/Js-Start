73ffeb9c1536f20fe2c40d109bb890ea97466f94
lakshyagarg08 <94711704+lakshyagarg08@users.noreply.github.com>
2024-09-22 16:13:02 +0000

    Strings

---
+++ 01_Basics/String.js
@@ -0,0 +1,13 @@
+const name = "lakshya"
+const repoCount =  50
+
+// console.log(name +  repoCount + " tell");  Outdated sytax
+
+console.log( `Hello my name is ${name} and my repo count is ${repoCount}`);
+//  Better way to write code
+// This is called string Interpolation
+
+const gameName = new String("Lakshyaa")
+console.log(gameName[0])
+
+
